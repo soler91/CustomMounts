b10c84397883430bc4496a20976363ddf6e03af5
Fruit <boskozarich@gmail.com>
2017-06-09 17:35:49 -0400

    Create index.js
    Now it only changes mount of the user
    Added invalid mounts
    Added system message

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 //Credits to Tera-Love and Saegusa
-//This is mostly copy paste
-
 const format = require('./format.js');
 
 module.exports = function CustomMounts(dispatch) {
     
-	let customMount = 0;
+	let target, customMount = 0;
+	
+	const INVALID = [4,100,105,106,107,108,109,110,111,112,
+			113,114,115,116,117,118,119,120,121,122,
+			123,124,125,126,127,128,129,130,131,132,
+			133,134,135,136,137,138,139,140,141,142,
+			143,144,145,146,147,148,149];
+	
+	dispatch.hook('S_LOGIN', 2, (event) => {cid = event.cid});
 	
 	dispatch.hook('S_MOUNT_VEHICLE', 1, (event) => {
+	console.log('Target:'+event.target+' CID:'+target)
+	
     if(customMount > 0 && customMount < 257) {
-		event.unk1 = customMount;
-        return true;
+		if(event.target.toString() === cid.toString()){
+			event.unk1 = customMount;
+			return true;
+		}
+		return true;
     }
 	});
 	
@@ -19,9 +30,28 @@ module.exports = function CustomMounts(dispatch) {
 		if(command[0] === '!mount') {
 			if (command.length > 1) {
 				customMount = parseInt(command[1]);
+				for (i = 0; i < INVALID.length; i++) {
+					if(INVALID[i] == customMount){
+					systemMsg('The value: '+customMount+' is invalid, try another');
+					customMount = 0;
+					}
+				}
+				systemMsg('Set to: '+customMount);
 			}
-			return false;			
+			return false;
 		}
 	});	
+	
+	function systemMsg(msg) {
+        dispatch.toClient('S_CHAT', 1, {
+            channel: 24,
+            authorID: 0,
+            unk1: 0,
+            gm: 0,
+            unk2: 0,
+            authorName: '',
+            message: ' (Custom-Mount) ' + msg
+        });
+    }
 	
 }
